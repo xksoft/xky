dcc1fa06bddf2125c1b616fce9f9c923a33d8f4e
xiaoxia <crackxia@gmail.com>
2019-03-03 18:10:26 +0800

    first commit

---
+++ Xky.Platform/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Shell;
+
+namespace Xky.Platform
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var wc = new WindowChrome
+            {
+                CaptionHeight = 40,
+                GlassFrameThickness = new Thickness(0),
+                CornerRadius = new CornerRadius(0),
+                UseAeroCaptionButtons = false,
+                ResizeBorderThickness = new Thickness(15)
+            };
+            Activated += MainWindow_Activated;
+            Deactivated += MainWindow_Deactivated;
+            SizeChanged += MainWindow_OnSizeChanged;
+            WindowChrome.SetWindowChrome(this, wc);
+        }
+
+
+        #region 界面UI事件
+
+        private void MainWindow_Deactivated(object sender, EventArgs e)
+        {
+            DropShadowEffect.Color = Color.FromArgb(204, 200, 200, 200);
+        }
+
+        private void MainWindow_Activated(object sender, EventArgs e)
+        {
+            DropShadowEffect.Color = Color.FromArgb(204, 0, 0, 0);
+        }
+
+        private void MainWindow_OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                var top = (e.NewSize.Height - SystemParameters.PrimaryScreenHeight) / 2 + SystemParameters.WorkArea.Top;
+                var left = (e.NewSize.Width - SystemParameters.WorkArea.Width) / 2 + SystemParameters.WorkArea.Left;
+                var right = (e.NewSize.Width - SystemParameters.WorkArea.Width) / 2 +
+                            SystemParameters.PrimaryScreenWidth - SystemParameters.WorkArea.Right;
+                var bottom = (e.NewSize.Height - SystemParameters.PrimaryScreenHeight) / 2 +
+                             SystemParameters.PrimaryScreenHeight - SystemParameters.WorkArea.Bottom;
+                MainGrid.Margin = new Thickness(left, top, right, bottom);
+                BtnRestore.Visibility = Visibility.Visible;
+                BtnMax.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                MainGrid.Margin = new Thickness(10);
+                BtnRestore.Visibility = Visibility.Collapsed;
+                BtnMax.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void btn_close(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_max(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState != WindowState.Maximized ? WindowState.Maximized : WindowState.Normal;
+        }
+
+
+        private void btn_min(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        #endregion
+    }
+}
