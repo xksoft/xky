97da5dbb115f8f299c1173585017b304dffc1a8f
xiakexiaoyi <24271786@qq.com>
2019-03-28 17:16:59 +0800

    表格测试数据

--- Xky.Platform/UserControl/Pages/MyTask.xaml.cs
+++ Xky.Platform/UserControl/Pages/MyTask.xaml.cs
@@ -22,27 +22,15 @@ namespace Xky.Platform.UserControl.Pages
     {
         public MyTask()
         {
+            Random R = new Random();
             InitializeComponent();
             List<XkyTask> list = new List<XkyTask>();
-            list.Add(new XkyTask { Id=1,Name="点赞加好友",State= "执行中" });
-            list.Add(new XkyTask { Id = 1, Name = "点赞加好友", State = "执行中" });
-            list.Add(new XkyTask { Id = 1, Name = "点赞加好友", State = "执行中" });
-            list.Add(new XkyTask { Id = 1, Name = "点赞加好友", State = "执行中" });
-            list.Add(new XkyTask { Id = 1, Name = "点赞加好友", State = "执行中" });
-            list.Add(new XkyTask { Id = 1, Name = "点赞加好友", State = "执行中" });
-            list.Add(new XkyTask { Id = 1, Name = "点赞加好友", State = "执行中" });
-            list.Add(new XkyTask { Id = 1, Name = "点赞加好友", State = "执行中" });
-            list.Add(new XkyTask { Id = 1, Name = "点赞加好友", State = "执行中" });
-            list.Add(new XkyTask { Id = 1, Name = "点赞加好友", State = "执行中" });
-            list.Add(new XkyTask { Id = 1, Name = "点赞加好友", State = "执行中" });
-            list.Add(new XkyTask { Id = 1, Name = "点赞加好友", State = "执行中" });
-            list.Add(new XkyTask { Id = 1, Name = "点赞加好友", State = "执行中" });
-            list.Add(new XkyTask { Id = 1, Name = "点赞加好友", State = "执行中" });
-            list.Add(new XkyTask { Id = 1, Name = "点赞加好友", State = "执行中" });
-            list.Add(new XkyTask { Id = 1, Name = "点赞加好友", State = "执行中" });
-            list.Add(new XkyTask { Id = 1, Name = "点赞加好友", State = "执行中" });
-            list.Add(new XkyTask { Id = 1, Name = "点赞加好友", State = "执行中" });
-            list.Add(new XkyTask { Id = 1, Name = "点赞加好友", State = "执行中" });
+            for (int i = 1; i < 9999; i++)
+            {
+                list.Add(new XkyTask { Id = i, Name = "点赞加好友", State = "执行中", Process = R.Next(500,9999) });
+            }
+           
+          
 
             datagrid_task.ItemsSource = list;
         }
@@ -51,9 +39,12 @@ namespace Xky.Platform.UserControl.Pages
         private int _id =1;
         private string _name = "加好友并点赞";
         private string _state = "执行中";
+        private int _process = 0;
+      
 
         public int Id { get => _id; set => _id = value; }
         public string Name { get => _name; set => _name = value; }
         public string State { get => _state; set => _state = value; }
+        public int Process { get => _process; set => _process = value; }
     }
 }
