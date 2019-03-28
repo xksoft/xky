48c07807d3224f34528e17856f599042d310c5ac
xiakexiaoyi <24271786@qq.com>
2019-03-28 16:55:54 +0800

    test

--- Xky.Platform/UserControl/Pages/MyTask.xaml.cs
+++ Xky.Platform/UserControl/Pages/MyTask.xaml.cs
@@ -23,6 +23,23 @@ namespace Xky.Platform.UserControl.Pages
         public MyTask()
         {
             InitializeComponent();
+            List<int[]> list = new List<int[]>();
+            list.Add(new int[] { 1, 2, 3 });
+            list.Add(new int[] { 2, 3, 4});
+            list.Add(new int[] { 3, 4, 5 });
+
+            int _col = list[0].Length;
+            int _row = list.Count;
+            for (int i = 0; i < _col; i++)
+            {
+                //datagrid_task.Columns.Add(new DataGridTextColumn
+                //{
+                //    Width = (Width - 30) / _col,
+                //    Header = $"{(char)(65 + i)}",
+                //    Binding = new Binding($"[{i.ToString()}]")
+                //});
+            }
+            datagrid_task.ItemsSource = list;
         }
     }
 }
