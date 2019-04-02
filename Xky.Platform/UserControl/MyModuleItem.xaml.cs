bfc992c59b62b13c4913f8da9ba751751401ecac
xiakexiaoyi <24271786@qq.com>
2019-04-02 17:52:11 +0800

    模块自定义控件

---
+++ Xky.Platform/UserControl/MyModuleItem.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Xky.Platform.UserControl
+{
+    /// <summary>
+    /// MyModuleItem.xaml 的交互逻辑
+    /// </summary>
+    public partial class MyModuleItem 
+    {
+        public MyModuleItem()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 是否正在执行
+        /// </summary>
+        public static readonly DependencyProperty IsRunningProperty = DependencyProperty.Register(
+            "IsRunning", typeof(bool), typeof(MyModuleItem), new PropertyMetadata(false));
+
+        public bool IsRunning
+        {
+            get => (bool)GetValue(IsRunningProperty);
+            set => SetValue(IsRunningProperty, value);
+        }
+
+        public static readonly DependencyProperty ModuleNameProperty = DependencyProperty.Register(
+            "ModuleName", typeof(string), typeof(MyModuleItem), new PropertyMetadata("未命名模块"));
+
+        public string ModuleName
+        {
+            get => (string)GetValue(ModuleNameProperty);
+            set => SetValue(ModuleNameProperty, value);
+        }
+
+    }
+}
