0622bb513d936368b414476b7fa1bf4832b69c4b
xiaoxia <crackxia@gmail.com>
2019-04-13 11:00:26 +0800

    加入模块基础类

---
+++ Xky.Core/XModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xky.Core
+{
+    /// <summary>
+    /// 侠客模块基础类
+    /// </summary>
+    public abstract class XModule : Object, ICloneable
+    {
+        /// <summary>
+        /// 克隆这个对象
+        /// </summary>
+        /// <returns></returns>
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+
+        /// <summary>
+        /// 模块名称
+        /// </summary>
+        /// <returns></returns>
+        public abstract string Name();
+
+        /// <summary>
+        /// 模块说明
+        /// </summary>
+        /// <returns></returns>
+        public abstract string Description();
+    }
+}
