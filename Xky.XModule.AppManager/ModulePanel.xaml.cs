cb0c19761a026769cb3ccf355d24ed9f14c71eca
xiakexiaoyi <24271786@qq.com>
2019-04-26 10:46:37 +0800

    应用管理器模块demo

--- Xky.XModule.AppManager/ModulePanel.xaml.cs
+++ Xky.XModule.AppManager/ModulePanel.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -32,15 +33,39 @@ namespace Xky.XModule.AppManager
         public Device device;
         public string CurrentDirectory = "/";
         public ObservableCollection<DeviceApp> DeviceApps = new ObservableCollection<DeviceApp>();
+        public Dictionary<string, string> PackageNames = new Dictionary<string, string>();
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
             ItemListBox.ItemsSource = DeviceApps;
-            LoadPackages();
 
+            new Thread(() =>
+            {
+                Assembly myAssembly = Assembly.GetExecutingAssembly();
+                Stream myStream = myAssembly.GetManifestResourceStream("Xky.XModule.AppManager.packages.txt");
+                byte[] bytes = new byte[myStream.Length];
+                myStream.Read(bytes, 0, bytes.Length);
+               string []infos= Encoding.UTF8.GetString(bytes).Split('\n');
+                foreach (string info in infos)
+                {
+                    string inf= info.Trim();
+                    if (inf.Length>0&&inf.Contains("|"))
+                    {
+                        string []infs = inf.Split('|');
+                        string appname = infs[0];
+                        string packagename = infs[1];
+                        if (!PackageNames.ContainsKey(packagename))
+                        {
+                            PackageNames.Add(packagename,appname);
+                        }
+                    }
+           
 
+                }
+                LoadPackages();
+            })
+            { IsBackground = true }.Start();
         }
-
-        private void btn_cancel_Click(object sender, RoutedEventArgs e)
+    private void btn_cancel_Click(object sender, RoutedEventArgs e)
         {
 
             Client.CloseDialogPanel();
@@ -58,7 +83,9 @@ namespace Xky.XModule.AppManager
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-           
+            DeviceApp deviceApp = (DeviceApp)((Button)sender).DataContext;
+            Console.WriteLine(deviceApp.PackageName);
+            Response res = device.ScriptEngine.RestartApp(deviceApp.PackageName);
         }
         public void LoadPackages()
         {
@@ -78,6 +105,13 @@ namespace Xky.XModule.AppManager
                             deviceApp.Type = "system";
                             int index = s.IndexOf("package:") + 8;
                             deviceApp.PackageName = s.Substring(index, s.Length - index);
+                            if (PackageNames.ContainsKey(deviceApp.PackageName))
+                            {
+                                deviceApp.Name = PackageNames[deviceApp.PackageName];
+                            }
+                            else {
+                                deviceApp.Name = deviceApp.PackageName;
+                            }
                             list.Add(deviceApp);
                         }
                     }
@@ -95,6 +129,14 @@ namespace Xky.XModule.AppManager
                             deviceApp.Type = "user";
                             int index = s.IndexOf("package:") + 8;
                             deviceApp.PackageName = s.Substring(index, s.Length - index);
+                            if (PackageNames.ContainsKey(deviceApp.PackageName))
+                            {
+                                deviceApp.Name = PackageNames[deviceApp.PackageName];
+                            }
+                            else
+                            {
+                                deviceApp.Name = deviceApp.PackageName;
+                            }
                             list.Add(deviceApp);
                         }
                     }
@@ -135,7 +177,23 @@ namespace Xky.XModule.AppManager
 
             
         }
-
+        private void MenuItem_Open_Click(object sender, RoutedEventArgs e)
+        {
+            DeviceApp deviceApp = (DeviceApp)((MenuItem)sender).DataContext;
+            Console.WriteLine(deviceApp.PackageName);
+            Response res = device.ScriptEngine.RestartApp(deviceApp.PackageName);
+          
+        }
+        private void MenuItem_Stop_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MenuItem_Clear_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        
+        private void MenuItem_Install_Click(object sender, RoutedEventArgs e)
+        {
+        }
         private void MenuItem_Delete_Click(object sender, RoutedEventArgs e)
         {
             //DeviceFile deviceFile = (DeviceFile)((MenuItem)sender).DataContext;
