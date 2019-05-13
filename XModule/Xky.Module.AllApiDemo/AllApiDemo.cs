7e701c32aa6e841203d9e108e799e230211568bb
xiakexiaoyi <24271786@qq.com>
2019-05-13 17:57:14 +0800

    增加所有api接口调用demo示例模块

---
+++ XModule/Xky.Module.AllApiDemo/AllApiDemo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xky.XModule.AllApiDemo;
+
+namespace Xky.XModule.AllApiDemo 
+{
+    public class AllApiDemo: Core.XModule
+    {
+        public override string Name()
+        {
+            return "模块API示例";
+        }
+        public override bool IsBackground()
+        {
+            return true;
+        }
+        public override byte[] Icon()
+        {
+            Assembly myAssembly = Assembly.GetExecutingAssembly();
+            Stream myStream = myAssembly.GetManifestResourceStream("Xky.XModule.AllApiDemo.logo.png");
+            byte[] bytes = new byte[myStream.Length];
+            myStream.Read(bytes, 0, bytes.Length);
+            return bytes;
+
+        }
+        public override string Description()
+        {
+            return "所有模块支持的API调用演示";
+        }
+
+        public override void Start()
+        {
+
+
+
+
+        }
+
+
+
+        public override bool ShowUserControl()
+        {
+            var panel = new ModulePanel();
+            panel.device = Device;
+            Core.Client.ShowDialogPanel(panel);
+            return true;
+
+        }
+    }
+}
---
+++ XModule/Xky.Module.AllApiDemo/ModulePanel.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Xky.Core;
+using Xky.Core.Model;
+using Xky.Core.UserControl;
+
+namespace Xky.XModule.AllApiDemo
+{
+    /// <summary>
+    /// ModulePanel.xaml 的交互逻辑
+    /// </summary>
+    public partial class ModulePanel : UserControl
+    {
+        public ModulePanel()
+        {
+            InitializeComponent();
+        }
+        public Device device;
+        private void Button_Close_Click(object sender, RoutedEventArgs e)
+        {
+            Client.CloseDialogPanel();
+        }
+       
+    
+
+
+       
+    }
+}
